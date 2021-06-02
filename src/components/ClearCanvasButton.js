24ec096512cf85dfaa99893852b16959e3644000
karishmas6 <carishmashukla28@gmail.com>
2021-06-03 00:57:10 +0530

    Initializing

---
+++ src/components/ClearCanvasButton.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ClearCanvasButton = () => {
+    return (
+        <div>
+            clear canvas button
+        </div>
+    )
+}
+
+export default ClearCanvasButton
