1b57ebdc9d16685c0857f085ba556602cb593d43
karishmas6 <carishmashukla28@gmail.com>
2021-06-03 01:38:45 +0530

    Clear canvas

--- src/components/ClearCanvasButton.js
+++ src/components/ClearCanvasButton.js
@@ -4,7 +4,7 @@ import { useCanvas } from '../contexts/CanvasContext'
 const ClearCanvasButton = () => {
   const { clearCanvas } = useCanvas()
 
-  return <button onClick={clearCanvas}>Clear Canvas</button>
+  return <button onClick={clearCanvas}>Clear</button>
 }
 
 export default ClearCanvasButton
--- src/contexts/CanvasContext.js
+++ src/contexts/CanvasContext.js
@@ -17,7 +17,7 @@ export const CanvasProvider = ({ children }) => {
     const context = canvas.getContext("2d")
     context.scale(2, 2);
     context.lineCap = "round";
-    context.strokeStyle = "black";
+    context.strokeStyle = "blue";
     context.lineWidth = 5;
     contextRef.current = context;
   };
