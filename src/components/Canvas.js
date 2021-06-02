1f0c13976a8d298325dde9e37f508c0808c5932a
karishmas6 <carishmashukla28@gmail.com>
2021-06-03 01:23:14 +0530

    Context

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -1,12 +1,19 @@
-import React from 'react'
+import React, {useRef, useEffect} from 'react'
 
 const Canvas = () => {
+
+
+    useEffect(() => {
+        
+    }, [])
+
     return (
 
         <canvas 
         onMouseDown={startDrawing}
         onMouseUp={finishDrawing}
         onMouseMove={draw}
+        ref = {canvasRef}
         />
     )
 }
